9e33d91afd6c923fe405639238ab0f0b90688273
Kelvinperezs <47196960+Kelvinperezs@users.noreply.github.com>
2020-01-24 16:17:08 -0500

    Update App.js

--- app-test/src/App.js
+++ app-test/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import People from "./People";
+import People from "./components/People";
 
 export default function App() {
   return (
